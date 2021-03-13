b445795120841dcd56dac94d23d7320ee398c5a0
Elisa <elisa.jimenez1@alu.uclm.es>
2021-03-13 15:50:35 +0100

    Clase note

---
+++ app/models/Note.rb
@@ -0,0 +1,14 @@
+require "date"
+
+class Note
+    attr_accessor :title, :text, :date
+
+    def initialize(title, text, date)
+        @title = title
+        @text = text
+        @date = date
+        
+        images = [] 
+
+    end 
+end
