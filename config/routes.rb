e2e03fb256dc6509e73bf5c4464504b8b3f38f9b
AARÓN RODRÍGUEZ-BARBERO GONZÁLEZ <Aharon1377@gmail.com>
2021-03-21 20:13:56 +0100

    hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #get "/notes", to: "notes#index"
- # root "notes#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "notes#index"
   resources :notes
 end
