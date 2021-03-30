14ffc702edcc0fb1b5e023f051d9cfd09f821702
AARÓN RODRÍGUEZ-BARBERO GONZÁLEZ <Aharon1377@gmail.com>
2021-03-30 18:08:21 +0200

    user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validations' do
     it "ensures the presence of username" do
-        user = User.create(username: "", name: "name", email: "email@email.com", password: "password", type: "USER")
+        user = User.create(username: "", name: "name", email: "email@email.com", password: "Password1", type: "USER")
         expect(user.save).to eq(false)
     end
     it "ensures the presence of name" do
-      user = User.create(username: "username", name: "", email: "email@email.com", password: "password", type: "USER")
+      user = User.create(username: "username", name: "", email: "email@email.com", password: "Password1", type: "USER")
       expect(user.save).to eq(false)
     end
     it "ensures the presence of email" do
-      user = User.create(username: "username", name: "name", email: "", password: "password", type: "USER")
+      user = User.create(username: "username", name: "name", email: "", password: "Password1", type: "USER")
       expect(user.save).to eq(false)
     end
     it "ensures the presence of password" do
@@ -19,45 +19,47 @@ RSpec.describe User, type: :model do
       expect(user.save).to eq(false)
     end
     it "ensures the presence of type" do
-      user = User.create(username: "username", name: "name", email: "email@email.com", password: "password", type: "")
+      user = User.create(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "")
       expect(user.save).to eq(false)
     end
     it "ensures username uniqueness" do
-      user = User.create!(username: "username", name: "name", email: "email@email.com", password: "password", type: "USER")
+      user = User.create!(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "USER")
       user.save!
-      user2 = User.create(username: "username", name: "name", email: "email2@email.com", password: "password", type: "USER")
+      user2 = User.create(username: "username", name: "name", email: "email2@email.com", password: "Password1", type: "USER")
       expect(user2.save).to eq(false)
       expect(user.delete).to eq(true)
     end
     it "ensures email uniqueness" do
-      user = User.create!(username: "username", name: "name", email: "email@email.com", password: "password", type: "USER")
+      user = User.create!(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "USER")
       user.save!
-      user2 = User.create(username: "username2", name: "name", email: "email@email.com", password: "password", type: "USER")
+      user2 = User.create(username: "username2", name: "name", email: "email@email.com", password: "Password1", type: "USER")
       expect(user2.save).to eq(false)
       expect(user.delete).to eq(true)
     end
     it "ensures type is valid" do
-      user = User.create(username: "username", name: "name", email: "email@email.com", password: "password", type: "type")
+      user = User.create(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "type")
       bool = (user.type == "USER" OR user.type == "ADMIN")
       expect(user.save).to eq(bool)
     end
     it "ensures email is valid" do
-      user = User.create(username: "username", name: "name", email: "email", password: "password", type: "type")
+      user = User.create(username: "username", name: "name", email: "email", password: "Password1", type: "type")
       bool = (user.email.include? "@" AND user.email.include? ".")
       expect(user.save).to eq(bool)
     end
     it "ensures password is valid" do
-      user = User.create(username: "username", name: "name", email: "email", password: "password", type: "type")
-      bool = (false)
-      expect(user.save).to eq(bool)
+      user = User.create(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "type")
+      expect(user.password =~ /[0-9]/).not_to eq(nil)
+      expect(user.password =~ /[a-z]/).not_to eq(nil)
+      expect(user.password =~ /[A-Z]/).not_to eq(nil)
+      expect(user.password.Length >= 8)
     end
     it "ensures username is valid" do
-      user = User.create(username: "username", name: "name", email: "email", password: "password", type: "type")
-      bool = (false)
+      user = User.create(username: "user name", name: "name", email: "email@email.com", password: "Password1", type: "type")
+      bool = (NOT user.username.contains? " ")
       expect(user.save).to eq(bool)
     end
     it "creates a user when validations are passed" do
-      user = User.create(username: "username", name: "name", email: "email@email.com", password: "password", type: "USER")
+      user = User.create(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "USER")
       expect(user.save!).to eq(true)
       expect(User.find(user.username).delete).to eq(true)
     end
@@ -67,7 +69,7 @@ RSpec.describe User, type: :model do
 
     subject { @user }
     before(:all) do
-      @user = User.create(username: "username", name: "name", email: "email@email.com", password: "password", type: "USER")
+      @user = User.create(username: "username", name: "name", email: "email@email.com", password: "Password1", type: "USER")
       @user.save!
     end
 
