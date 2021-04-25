d44379fc4a08a7d607ac545dcb20e2482ee6379a
Elisa <elisa.jimenez1@alu.uclm.es>
2021-04-25 18:52:52 +0200

    class collection

---
+++ app/models/collection.rb
@@ -0,0 +1,17 @@
+require 'uuidtools'
+require 'rails/mongoid'
+
+class Collection
+    include Mongoid::Document
+    include Mongoid::Timestamps::Created
+    store_in collection: "collections", database: "NotesWSD2021"
+
+    field :_id, type: String, default: ->{ SecureRandom.uuid.to_s} 
+    field :title, type: String
+
+    validates_presence_of :_id, :title
+    validates_uniqueness_of :_id
+
+    has_many : notes
+    belongs_to :user
+end
