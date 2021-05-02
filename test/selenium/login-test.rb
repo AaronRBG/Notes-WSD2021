71800bdbf9e6840f9aba22932d325460b88e107d
Elisa <elisa.jimenez1@alu.uclm.es>
2021-05-02 20:28:46 +0200

    selenium login funciona

--- test/selenium/login-test.rb
+++ test/selenium/login-test.rb
@@ -5,24 +5,18 @@ require "test/unit"
 class LoginClass < Test::Unit::TestCase
  
   def setup
-    Selenium::WebDriver::Chrome.driver_path = File.join(File.absolute_path('', File.dirname("C:\Users\Eli\OneDrive\Escritorio")),"Drivers","chromedriver.exe")
     @driver = Selenium::WebDriver.for :chrome
-    @driver.get('localhost:3000')
+    @driver.get('https://noteswsd2021.herokuapp.com/')
     @driver.manage.window.maximize    
   end
  
  
-  def teardown
-    @driver.quit
-  end
- 
  
   def test_login
-    @driver.find_element(:name, "username").send_keys "admin"
-    @driver.find_element(:password, "password").send_keys "Admin1234"
-    @driver.find_element(:id, "submit").click
+    @driver.find_element(:name, "username").send_keys "Admin"
+    @driver.find_element(:name, "password").send_keys "Admin1234"
+    @driver.find_element(:name, "commit").click
     sleep 0.3
-    assert(@driver.find_element(:id => "loggedin").text.include?("You Are Logged in"),"Assertion Failed")
-    @driver.find_element(:id, "logout").click
+    assert(@driver.find_element(:class => "user-name").text.include?("Admin"),"Assertion Failed")
   end
 end
