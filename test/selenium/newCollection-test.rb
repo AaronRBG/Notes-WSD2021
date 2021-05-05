d0ca097b0cdf134b47c6a2dd61e952ab43053765
Diego <diegoarche@hotmail.com>
2021-05-05 17:50:37 +0200

    new Collection completed, Add note to collection and show it

--- test/selenium/newCollection-test.rb
+++ test/selenium/newCollection-test.rb
@@ -31,20 +31,28 @@ class Collection_Class < Test::Unit::TestCase
     assert(@driver.find_element(:id => "table-index").text.include?("Nueva coleccion Selenium"),"Assertion Failed")
 
 
+    #Se añade nota a la coleccion de la ultima posicion que sera la que acabamos de añadir
+    table=@driver.find_element(:id => "table-index")
+    row_count=table.all(:css, 'tr').size
+    fila=@driver.find_elements(:xpath => "//table[@id='table-index']/tbody/tr")[row_count-1]
+    fila.find_element(:id, "btn3-index").click
+    sleep 1
+   
+   
+    #obtengo la primera fila donde estara la nota y el boton de añadir
+    fila=@driver.find_elements(:xpath => "//table[@id='table-add']/tbody/tr")[1]
+    #obtengo el nombre de la primera nota para luego comprobar si la añade bien
+    nombre=fila.text.split("\n")[0]
+    #añado la primera nota 
+    fila.find_element(:id, "tr2-add").click
     sleep 1
 
-    #Se añade nota a la coleccion
-    @driver.get('https://noteswsd2021.herokuapp.com/notecollections/')
-    @driver.find_element(:id, "btn3-index").click
-    @driver.find_element(:id, "tr2-add").click
-    sleep 0.3
-
-    #Se comprueba el show notes viendo si se ha añadido la anterior nota
-    @driver.get('https://noteswsd2021.herokuapp.com/notecollections/')
-    @driver.find_element(:id, "btn2-index").click
-    sleep 0.3
-
-    assert(@driver.find_element(:id => "table-body-show").text.include?("Note 1"),"Assertion Failed")
+   #Se comprueba el show notes viendo si se ha añadido la anterior nota
+    fila=@driver.find_elements(:xpath => "//table[@id='table-index']/tbody/tr")[row_count-1]
+    fila.find_element(:id, "btn2-index").click
+    sleep 1
+    nota=@driver.find_elements(:xpath => "//table[@id='table-show']/tbody/tr")[0]
+    assert(nota.find_element(:id => "td-show").text.include?(nombre),"Assertion Failed")
 
 
 
